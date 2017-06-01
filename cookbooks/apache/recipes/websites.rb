c90f82655d37cc958e17df4e7220cf27ad34489c
feraszah <feras.zahid@gmail.com>
2017-06-01 03:39:04 +0000

    chef recipe update

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -2,3 +2,9 @@ file 'default www' do
 	path '/var/www/html/index.html'
 	content 'The most boring default content ever'
 end
+
+webnodes = search('node', 'role:web')
+
+webnodes.each do |node|
+	puts node
+end
